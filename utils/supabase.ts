c0a7c145059d17acc654544c99c52c2008160009
FranciscoArias10 <fariasp2@unemi.edu.ec>
2026-01-21 20:34:37 -0500

    actualizacion de seguridad

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://pemxcakymzhmemnkzwwf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlbXhjYWt5bXpobWVtbmt6d3dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMjcyNjIsImV4cCI6MjA4NDYwMzI2Mn0.DIl-xpQ5aJbaZ1nQt_DO-n_siHgQ1BNqFoVu96CdVwM';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
